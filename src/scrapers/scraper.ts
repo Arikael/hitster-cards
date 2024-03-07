078c813656f0b912066f1e4d9161502923ab4a8e
Arikael <mhaeberle@mailbox.org>
2024-03-07 21:37:19 +0100

    replace console.log

--- src/scrapers/scraper.ts
+++ src/scrapers/scraper.ts
@@ -11,7 +11,7 @@ const availableScrapers: ScraperSource[] = [
 const maxSongsToInsertPerBatch = 20
 
 export const scrape = async (source: string, detailUrl?: string) => {
-    console.log(chalk.green.bold('getting songs from ' + source))
+    console.info(chalk.green.bold('getting songs from ' + source))
     const db = new SongDatabase()
     await db.initDatabase()
     const scraperSource = availableScrapers.find(x => x.name === source)
@@ -36,7 +36,7 @@ const fullScrape = async(scraperSource: ScraperSource, db: SongDatabase) => {
     if (years.length === 0) {
         console.error(chalk.red('unable to get any years'))
     }
-    console.log(JSON.stringify(years))
+
     console.info(chalk.blue(`got years ${years[0].year} - ${years[years.length - 1].year}`))
 
     for (let scrapedYear of years) {
@@ -45,7 +45,7 @@ const fullScrape = async(scraperSource: ScraperSource, db: SongDatabase) => {
             return (<ScrapedSong>x).scrapePlayUrl && (<ScrapedSong>x).playUrl
         })
 
-        console.log(chalk.blue(`${scrapedYear.year} -> got ${songsTitles.length} songs`))
+        console.info(chalk.blue(`${scrapedYear.year} -> got ${songsTitles.length} songs`))
 
         for (let i = 0; i < songsTitles.length; i++) {
             const currentSong = songsToGetDetail[i]
